bc435d1986884088c42343ff0d0be380234d9308
smrk2415 <smarakgartia2415@gmail.com>
2025-12-24 20:47:26 +0530

    fixed some bugs

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -20,7 +20,7 @@ const renderText = (text, className, baseWeight = 400) => {
 };
 
 const setuptextHover = (container, type) => {
-    if (!container) return;
+    if (!container) return () => {};
 
     const letters = container.querySelectorAll("span");
     const { min, max, default: base } = FONT_WEIGHTS[type];
@@ -84,7 +84,7 @@ const Welcome = () => {
         {renderText("portfolio", "text-9xl italic font-georama")}
     </h1>
     <div className="small-screen">
-        <p>This Portfolio is designed for desktop/tabled screens only.</p>
+        <p>This Portfolio is designed for desktop/tablet screens only.</p>
     </div>
   </section>
 }
