ed5f5b8b88604381a47a21a7676fd8adb8dd9062
smrk2415 <smarakgartia2415@gmail.com>
2025-12-26 20:17:52 +0530

    Add Zustand store for window management and update dependencies

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import { Tooltip } from 'react-tooltip';
 import { dockApps } from '#constants';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import useWindowStore from '#store/windows';
 
 function Dock() {
+  const { openWindow, closeWindow, windows } = useWindowStore();
   const dockRef = useRef(null);
 
   useGSAP(() => {
@@ -57,7 +59,23 @@ function Dock() {
     };
   });
 
-  const toggleApp = (app) => {};
+  const toggleApp = (app) => {
+    if (!app.canOpen) return(() => {});
+
+    const window = windows[app.id];
+
+    if (!window) {
+      console.error(`Window not found for app: ${app.id}`);
+      return;
+    }
+
+    if (window.isOpen) {
+      closeWindow(app.id);
+    } else {
+      openWindow(app.id);
+    }
+
+  };
 
   return (
     <section id="dock">
